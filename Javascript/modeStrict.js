f74410bf29b75256f258f4339dbe674a4926c65f
MuhammadFarhan200 <farhan_200@smkassalaambandung.sch.id>
2022-08-31 15:01:17 +0700

    Belajar Javascript

--- Javascript/modeStrict.js
+++ Javascript/modeStrict.js
@@ -22,7 +22,7 @@
 "use strict";
 x = 9;       // ini akan dianggap error karena variabel belum dideklarasikan
 showAngka();   // ini akan dianggap error karena function belum dideklarasikan
- 
+
 function showAngka() {
   var x = 9;
   alert(x);
@@ -30,10 +30,10 @@ function showAngka() {
 
 a = 9;       // ini tidak dianggap error karena tidak kena mode Strict
 showAngka();   // ini tidak dianggap error karena tidak kena mode Strict
- 
+
 function showAngka() {
-  “use strict”;
-   a = 9; // ini akan dianggap error karena variabel belum dideklarasikan
+  "use strict";
+  a = 9; // ini akan dianggap error karena variabel belum dideklarasikan
   alert(a);
 }
 
