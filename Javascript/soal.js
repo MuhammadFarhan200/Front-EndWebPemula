7fc58cccb422cca5a714825d39dce17c034323fb
MuhammadFarhan200 <farhan_200@smkassalaambandung.sch.id>
2022-09-02 14:37:27 +0700

    Belajar Javascript

--- Javascript/soal.js
+++ Javascript/soal.js
@@ -25,17 +25,22 @@
 // let numbers = [12, 1, 23, 2];
 // console.log(numbers[numbers.length-1])
 
-let score = 83;
+// let score = 83;
 
-if (score >= 90) {
-    result = "Selamat! Anda mendapatkan Nilai A";
-} else if (score >= 80) {
-    result = "Anda mendapatkan Nilai B";
-} else if (score >= 70) {
-    result = "Anda mendapatkan Nilai C";
-} else if (score >= 60) {
-    result = "Anda mendapatkan Nilai D";
-} else if (score < 60) {
-    result = "Anda mendapatkan Nilai E";
-}
-console.log(result);
+// if (score >= 90) {
+//     result = "Selamat! Anda mendapatkan Nilai A";
+// } else if (score >= 80) {
+//     result = "Anda mendapatkan Nilai B";
+// } else if (score >= 70) {
+//     result = "Anda mendapatkan Nilai C";
+// } else if (score >= 60) {
+//     result = "Anda mendapatkan Nilai D";
+// } else if (score < 60) {
+//     result = "Anda mendapatkan Nilai E";
+// }
+// console.log(result);
+
+
+if (123 === '123') console.log('Dicoding Indonesia')
+else if ('123' == 123) console.log('Bandung Kota')
+else console.log('Front-End Web Developer');
