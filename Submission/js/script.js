56a8766fd4a5d1d15325b3c8fb3fad6e6be1d104
MuhammadFarhan200 <farhan_200@smkassalaambandung.sch.id>
2022-09-05 08:31:34 +0700

    Belajar Javascript

--- Submission/js/script.js
+++ Submission/js/script.js
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function () {
         checkButton();
     });
 
-    formSearch.addEventListener("submit", function (event) {
+    formSearch.addEventListener("keyup", function (event) {
         event.preventDefault();
         bookSearch();
     });
